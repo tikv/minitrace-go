14f65f14bd22c1b170bc3e46eb7b3a5a2d24cde2
zhongzc <zhongzc_arch@outlook.com>
2020-06-10 12:29:04 +0800

    add unit test
    Signed-off-by: zhongzc <zhongzc_arch@outlook.com>

--- minitrace_test.go
+++ minitrace_test.go
@@ -3,6 +3,7 @@ package minitrace
 import (
     "context"
     "fmt"
+    "sync"
     "testing"
 )
 
@@ -27,5 +28,43 @@ func tracedFunc(l int, b *testing.B) {
     spanSets := handle.Finish()
     if l != len(spanSets[0].Spans) {
         b.Fatalf("expected length %d, got %d", l, len(spanSets[0].Spans))
+    }
+}
+
+func TestMiniTrace(t *testing.T) {
+    ctx, handle := TraceEnable(context.Background(), 0)
+    var wg sync.WaitGroup
+
+    for i := 1; i < 5; i++ {
+        ctx, handle := NewSpanWithContext(ctx, uint32(i))
+        wg.Add(1)
+        go func(prefix int) {
+            ctx, handle := NewSpanWithContext(ctx, uint32(prefix))
+            for i := 0; i < 5; i++ {
+                wg.Add(1)
+                go func(prefix int) {
+                    handle := NewSpan(ctx, uint32(prefix))
+                    handle.Finish()
+                    wg.Done()
+                }((prefix + i) * 10)
+            }
+            handle.Finish()
+            wg.Done()
+        }(i * 10)
+        handle.Finish()
+    }
+
+    wg.Wait()
+    spanSets := handle.Finish()
+    if len(spanSets) != 25 {
+        t.Fatalf("length of spanSets expected %d, but got %d", 25, len(spanSets))
+    }
+
+    sum := 0
+    for _, spanSet := range spanSets {
+        sum += len(spanSet.Spans)
+    }
+    if sum != 29 {
+        t.Fatalf("count of spans expected %d, but got %d", 29, sum)
     }
 }
