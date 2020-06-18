a8e2b164dcb7a9606aa0c39312794532c2279c28
zhongzc <zhongzc_arch@outlook.com>
2020-06-18 16:16:29 +0800

    recorrect order
    Signed-off-by: zhongzc <zhongzc_arch@outlook.com>

--- buffer_list.go
+++ buffer_list.go
@@ -13,25 +13,28 @@ type buffer struct {
 
 type bufferList struct {
     head *buffer
+    tail *buffer
     len  int
 }
 
 var bufferPool = &sync.Pool{New: func() interface{} { return &buffer{} }}
 
 func newBufferList() *bufferList {
-    return &bufferList{nil, 0}
+    n := bufferPool.Get().(*buffer)
+    return &bufferList{n, n, 0}
 }
 
 func (bl *bufferList) slot() *Span {
     idx := bl.len & ((1 << POW) - 1)
     if idx == 0 {
         n := bufferPool.Get().(*buffer)
-        n.next = bl.head
-        bl.head = n
+
+        bl.tail.next = n
+        bl.tail = n
     }
 
     bl.len += 1
-    return &bl.head.array[idx]
+    return &bl.tail.array[idx]
 }
 
 func (bl *bufferList) collect() []Span {
@@ -39,27 +42,26 @@ func (bl *bufferList) collect() []Span {
         return nil
     }
 
-    h := bl.head
-    next := h.next
+    h := bl.head.next
+    bufferPool.Put(bl.head)
+    bl.head = nil
+
     res := make([]Span, bl.len, bl.len)
 
-    size := 1 << POW
-    cursor := bl.len & (size - 1)
-    if cursor == 0 {
-        cursor = size
-    }
-
-    copy(res[:cursor], h.array[:cursor])
-    bufferPool.Put(h)
-    h = next
-
-    for h != nil {
-        copy(res[cursor:cursor+size], h.array[:size])
-        cursor += size
-        next = h.next
+    remainingLen := bl.len
+    sizePerBuffer := 1 << POW
+    for remainingLen > sizePerBuffer {
+        cursor := bl.len - remainingLen
+        copy(res[cursor:cursor+sizePerBuffer], h.array[:])
+        remainingLen -= sizePerBuffer
+        n := h.next
         bufferPool.Put(h)
-        h = next
+        h = n
     }
+
+    cursor := bl.len - remainingLen
+    copy(res[cursor:], h.array[:remainingLen])
+    bufferPool.Put(h)
 
     return res
 }
