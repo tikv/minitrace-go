a9c25a8d91beb30388b7801ee3b8878cb5964f24
Zhenchi <zhongzc_arch@outlook.com>
2020-06-19 11:40:42 +0800

    adapt to pingcap-incubator (#1)
    Signed-off-by: zhongzc <zhongzc_arch@outlook.com>

--- time.go
+++ time.go
@@ -1,3 +1,5 @@
+// Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
+
 package minitrace
 
 import (
@@ -10,10 +12,18 @@ func nanotime() int64
 //go:linkname walltime runtime.walltime
 func walltime() (sec int64, nsec int32)
 
+// Standard library's `time.Now()` will invoke two syscalls in Linux, one for `CLOCK_REALTIME`,
+// another for `CLOCK_MONOTONIC`. In our case, we'd like to separate these two calls to measure
+// time for performance purpose.
+// `nanotime()` is identical to Linux's `clock_gettime(CLOCK_MONOTONIC, &ts)`
 func monotimeNs() uint64 {
     return uint64(nanotime())
 }
 
+// Standard library's `time.Now()` will invoke two syscalls in Linux, one for `CLOCK_REALTIME`,
+// another for `CLOCK_MONOTONIC`. In our case, we'd like to separate these two calls to measure
+// time for performance purpose.
+// `nanotime()` is identical to Linux's `clock_gettime(CLOCK_REALTIME, &ts)`
 func realtimeNs() uint64 {
     sec, nsec := walltime()
     return uint64(sec*1_000_000_000 + int64(nsec))
