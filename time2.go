8e6316bb4097f0b7832633a04e5935eccc8ad208
tangenta <tangenta@126.com>
2022-09-23 17:15:13 +0800

    time: fix build for linux/amd64 (#33)
    * time: fix build for linux/amd64
    
    * fix linkname
    
    * refine comment

--- time2.go
+++ time2.go
@@ -1,4 +1,4 @@
-// Copyright 2020 PingCAP, Inc.
+// Copyright 2022 PingCAP, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,9 +10,8 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//go:build !(windows || (linux && amd64))
-// +build !windows
-// +build !linux !amd64
+//go:build windows || (linux && amd64)
+// +build windows linux,amd64
 
 package minitrace
 
@@ -23,8 +22,8 @@ import (
 //go:linkname nanotime runtime.nanotime
 func nanotime() int64
 
-//go:linkname walltime runtime.walltime
-func walltime() (sec int64, nsec int32)
+//go:linkname time_now time.now
+func time_now() (sec int64, nsec int32, mono int64)
 
 // Standard library's `time.Now()` will invoke two syscalls in Linux. One is `CLOCK_REALTIME` and
 // another is `CLOCK_MONOTONIC`. In our case, we'd like to separate these two calls to measure
@@ -36,9 +35,9 @@ func monotimeNs() uint64 {
 
 // Standard library's `time.Now()` will invoke two syscalls in Linux. One is `CLOCK_REALTIME` and
 // another is `CLOCK_MONOTONIC`. In our case, we'd like to separate these two calls to measure
-// time for performance purpose.
-// `unixtimeNs()` is identical to Linux's `clock_gettime(CLOCK_REALTIME, &ts)`
+// time for performance purpose. However, the walltime() is removed from runtime in go 1.17.
+// We have to use the function time_now().
 func unixtimeNs() uint64 {
-	sec, nsec := walltime()
+	sec, nsec, _ := time_now()
 	return uint64(sec*1_000_000_000 + int64(nsec))
 }
